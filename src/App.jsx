3a4fe5af06ecf2126b6a9edf5415d4f9a8fa9b34
deuteragonistDev <pjn268vjds@privaterelay.appleid.com>
2026-08-15 17:54:59 -0300

    Fix issue: aparecia PageNotFound ao ser redirecionado do git para o portifolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import Topbar from './Components/Topbar';
-import { Routes, Route } from 'react-router-dom';
-import { Hero } from './pages/Hero.jsx';
+import {Route, Routes} from 'react-router-dom';
+import {Hero} from './pages/Hero.jsx';
 import {Home} from "./pages/home.jsx";
 
 function App() {
-  return (
-    <div className={'bg-gray-100 dark:bg-gray-800 min-h-screen transition-colors'}>
-      <Topbar />
+    return (
+        <div className={'bg-gray-100 dark:bg-gray-800 min-h-screen transition-colors'}>
+            <Topbar/>
 
-      <Routes>
-        <Route path={'/'} element={<Home />} />
-        <Route path={'/hero'} element={<Hero />} />
-        <Route path={'*'} element={<h1>404 - Page Not Found</h1>} />
-      </Routes>
-    </div>
-  );
+            <Routes>
+                <Route path={'/'} element={<Home/>}/>
+                <Route path={'/hero'} element={<Hero/>}/>
+                <Route path={'*'} element={<h1>404 - Page Not Found</h1>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
